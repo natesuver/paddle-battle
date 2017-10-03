8a0fe721a2e4924b93bdc7ef1560f71aa3bc447c
Mike Peal <michaelcpeal@gmail.com>
2017-10-03 13:06:08 -0400

    Convert home page to php, add CSS file

---
+++ home/home.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="home.css" />
+	<title>Welcome to Paddle Battle</title>
+</head>
+
+<body>
+<div class="container">
+	<div class="row" id="top_row">
+		<div class="col-md-12 text-center">
+			<h3>Welcome to</h3>
+			<h1>Paddle Battle</h1>
+		</div>
+	</div>
+	<div id="login_form_wrapper">
+		<div class="row text-center">
+			<form action = "" method="POST">
+				<div class="row">
+					<div class="col-md-12">
+						Username: <input id="login_username" type='text' name='username'></input>
+					</div>
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-md-12">
+						Password:   <input id="login_pword" type='password' name='password'></input>
+					</div>
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-md-12">
+						<input id ="login_button" type='submit' value="Log In" name='submit'></input>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+	<br>
+	<br>
+	<div class="row">
+		<div id="feedback_display" class="col-md-12">
+		</div>
+	</div>
+	<div>
+		<div class="row text-center">
+			<div class="col-md-12">
+				Not registered?  Sign up <a href="/register.html">here!</a>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
