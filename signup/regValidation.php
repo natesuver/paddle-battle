4105a7a61ca0a78990db64e112b646986b5da77c
Mike Peal <michaelcpeal@gmail.com>
2017-10-03 19:46:42 -0400

    first pass at script test on DB table

--- signup/regValidation.php
+++ signup/regValidation.php
@@ -1,20 +1,15 @@
 <? php
-\
 /*
 THIS SCRIPT IS A CHECK FOR WHETHER A SUBMITTED USERNAME ALREADY EXISTS
 returns array
 */
 $username = $_POST['username'];
 $pw = $_POST['pw'];
-
 //$conn dependent on our DB host settings
-$conn = '???';
-
+$conn = new mysqli('mysql5.gear.host', 'paddlebattle', 'Nu7n472~Cj!k', 'paddlebattle');
 //$query dependent on table name, column names
-$query = "SELECT * FROM db.table WHERE username = $username";
-
+$query = "SELECT * FROM paddlebattle.users WHERE username = ".$username;
 $res= $conn->query($query);
-
 if($res->num_rows > 0){
 	return array(
 		'isValid'=> false,
@@ -22,9 +17,8 @@ if($res->num_rows > 0){
 		);
 }else{
 	//$insert dependent on table name
-	$insert = "INSERT into db.table VALUES ($username, $pw, true)";
-
-	$ins = $conn->execute($insert);\
+	$insert = "INSERT into paddlebattle.users ('username', 'pass', 'isLoggedIn') VALUES (".$username.", ".$pw.", true)";
+	$ins = $conn->execute($insert);
 	
 	if($ins){
 		return array(
