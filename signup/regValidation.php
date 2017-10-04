efc142823f3b1291eb6ad52186e9f77a18a0c07a
Mike Peal <michaelcpeal@gmail.com>
2017-10-03 23:50:20 -0400

    signup script should work now

--- signup/regValidation.php
+++ signup/regValidation.php
@@ -1,35 +1,46 @@
-<? php
+<?php
 /*
 THIS SCRIPT IS A CHECK FOR WHETHER A SUBMITTED USERNAME ALREADY EXISTS
 returns array
 */
-$username = $_POST['username'];
-$pw = $_POST['pw'];
+$username = $_POST["username"];
+$pw = $_POST["password"];
+
 //$conn dependent on our DB host settings
-$conn = new mysqli('mysql5.gear.host', 'paddlebattle', 'Nu7n472~Cj!k', 'paddlebattle');
+$conn = mysqli_connect('mysql5.gear.host', 'paddlebattle', 'Nu7n472~Cj!k', 'paddlebattle');
+
 //$query dependent on table name, column names
-$query = "SELECT * FROM paddlebattle.users WHERE username = ".$username;
-$res= $conn->query($query);
-if($res->num_rows > 0){
-	return array(
+$query = "SELECT * FROM users WHERE username = '$username'";
+
+$res = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($res) != 0){
+		$data = array(
 		'isValid'=> false,
 		'feedback'=> 'Username unavailable.'
 		);
+		echo json_encode($data);
+		exit;
 }else{
-	//$insert dependent on table name
-	$insert = "INSERT into paddlebattle.users ('username', 'pass', 'isLoggedIn') VALUES (".$username.", ".$pw.", true)";
-	$ins = $conn->execute($insert);
+	$insert = "INSERT into users (username, pass, isLoggedIn) VALUES ('$username', '$pw', true)";
+	$ins = $conn->query($insert);
 	
 	if($ins){
-		return array(
-		'isValid'=> true,
-		'feedback'=> 'Success!  You will now be redirected to the game lobby.'
-		);
+		$data = array(
+			'isValid' => true,
+			'feedback' => "All good!"
+			);
+
+		echo json_encode($data);
+		exit;
 	}else{
-		return array(
+		$data = array(
 			'isValid'=> false,
 			'feedback'=> "Ut-oh! Something went wrong. We're really embarrassed now."
 			);
+		echo json_encode($data);
+		exit;
 	}
 }
+
 ?>
