8c241ba077acef1824a0243c576ae9aa9b28265a
Nate Suver <nate.suver@gmail.com>
2017-10-07 22:51:29 -0400

    testing

--- test/countdown.spec.js
+++ test/countdown.spec.js
@@ -2,4 +2,7 @@ describe("I will know this test is good when", function() {
     it("i do a thing and it works good", function() {
         expect("1").toBe("1");
     });
+    it("i do a 2nd thing and it works good too", function() {
+        expect("2").toBe("2");
+    });
 });
