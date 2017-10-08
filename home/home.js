7f5df5425a026053babb299e4ea6754ae70adbcd
Nate Suver <nate.suver@gmail.com>
2017-10-08 19:32:51 -0400

    remove ball-pacolypse from login page

--- home/home.js
+++ home/home.js
@@ -2,11 +2,6 @@ $('#submit_button').click(function(){
 	submit();
 });
 
-var canv = document.createElement('canvas');
-canv.id = 'board';
-
-document.body.appendChild(canv); // adds the canvas to the body element
-
 function updateFeedback(text, color='black'){
 	$('#feedback_display').text(text);
 	$('#feedback_display').css('color', color);
@@ -35,69 +30,5 @@ function submit(){
 		}
 	});
 }
-function createBall(Physics, i) {
-	var ball = Physics.body('circle', {
-        x: 250+i, //starting x coordinate
-        y: 250+i, //starting y coordinate
-        vx: 0.2	, //starting x velocity
-        vy: 0.2, //starting y velocity
-        radius: 15,
-        cof: 0.00, //drag coefficient
-        styles: {
-            fillStyle: 'black'
-        }
-    });
-	ball.id = "ball";
-	return ball;
-}
-$(document).ready(function() { //begin rendering only when the document has completely loaded
-	var board = document.getElementById("board");
 
-	var width = $(board).width();
-	var height = $(board).height();
-    world = Physics();
-    var renderer = Physics.renderer("canvas",{
-        el: "board",	// canvas element id
-        width: width,		// canvas width
-        height: height,		// canvas height
-        meta: false,		// setting it to "true" will display FPS
-        styles: {
-            // set colors for the circle bodies
-            'circle' : {
-                strokeStyle: 'white',
-                lineWidth: 1,
-                fillStyle: 'blue',
-                angleIndicator: 'none'
-            }
-        }
-    });	
-    world.add(renderer);
-
-	for (i=0;i<100;i++) {
-		world.add( createBall(Physics,i) );
-	}
-    
-
-	world.add( Physics.behavior('sweep-prune') );
-    var bounds = Physics.aabb(0, 0, width, height); //define the boundaries of our container.
-    world.add( Physics.behavior('edge-collision-detection', {
-        aabb: bounds,
-        restitution: 1
-    }) );
-    world.add( Physics.behavior('body-collision-detection') );
-    // ensure objects bounce when edge collision is detected
-    world.add( Physics.behavior('body-impulse-response') );
-
-    Physics.util.ticker.on(function( time, dt ){
-        world.step( time );
-    });
-
-    // start the ticker
-    Physics.util.ticker.start();
-    world.on('step', function(){
-        world.render();
-    });
-
-    
-  });
 
--- home/home.php
+++ home/home.php
@@ -59,6 +59,3 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="home.js"></script>
-<script src="http://wellcaffeinated.net/PhysicsJS/assets/scripts/vendor/physicsjs-0.6.0/physicsjs-full-0.6.0.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-crossorigin="anonymous"></script>
