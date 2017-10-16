cc08c4ea62fab73de7ab08da34bbc2964abc2733
Nate Suver <nate.suver@gmail.com>
2017-10-16 07:41:04 -0400

    make all the things better

--- game/countdown.js
+++ game/countdown.js
@@ -14,9 +14,9 @@ class Countdown {
             that.element.fadeIn(0);
             that.setCountdownName(increment, that.element);
             that.element.fadeOut(that.countdownDuration);
-            if (increment>0) {
+           // if (increment>0) { //terminating conditions remove to blow test
                 that.doCount(--increment);
-            }
+           // }
         },that.countdownDuration);
     }
     
