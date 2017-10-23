21ef992f83612e09288ca467b236c93ad32b6301
Mike Peal <michaelcpeal@gmail.com>
2017-10-23 19:08:28 -0400

    Leaving a team php script should work.
    Tested using username='stif'

--- game/leaveTeam.php
+++ game/leaveTeam.php
@@ -14,32 +14,40 @@ function returnFalse()
 	echo json_encode($response);
 }
 
-$conn = getConnection();
-
 /*
 first, get the user id for the user
 */
-$conn = getConnection();
+$username = $_SESSION['username'];
+//$username = 'stif'; //test
 
-//$query = "SELECT id from users where username = ".$_SESSION['username'];
-$query = "SELECT id from users where username = 'derf'"; //testing
+$conn = getConnection();
+$query = "SELECT id from users where username = '$username'";
 $res = mysqli_query($conn, $query);
 
-$user_id = ??;
-
-//$game_id = $_POST['game_id'];
-$game_id = 1; //testing
-
-$query = "DELETE FROM users where username = 'derf' and game_id = '$game_id'";
-$del = mysqli_query($conn, $query);
-
-if(!$del){
+if(!$res){
 	returnFalse();
 	exit;
 }else{
-	$response = array(
+	$user_id = '';
+
+	//assign $user_id
+	while($row = mysqli_fetch_row($res)){
+		$user_id = $row[0];
+	}
+
+	$game_id = $_POST['game_id'];
+	//$game_id = 1;
+
+	$query = "DELETE FROM teams where user_id = '$user_id' and game_id = '$game_id'";
+	$del = mysqli_query($conn, $query);
+	if(!$del){
+		returnFalse();
+		exit;
+	}else{
+		$response = array(
 			"success" => true
 		);
-	echo json_encode($response);
-	exit;
+		echo json_encode($response);
+		exit;
+	}
 }
