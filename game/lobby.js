9d8374f9286a6e0573a2e641e916eecbb4ed9e0e
Mike Peal <michaelcpeal@gmail.com>
2017-10-23 16:37:01 -0400

    Added player population by game_id

--- game/lobby.js
+++ game/lobby.js
@@ -2,10 +2,12 @@ var select = $('select');
 var game_id;
 
 $(document).ready(function(){
-	fillSelect();
+	fillSelectBox();
+
+	bindEvents();
 });
 
-function fillSelect()
+function fillSelectBox()
 {
 	$.ajax({
 		type: "POST",
@@ -28,5 +30,48 @@ function fillSelect()
 		error: function(response){
 			alert("Something went wrong.  We REALLY suck. =(");
 		}
+	});
+}
+
+function populatePlayers()
+{
+	//clear out ordered lists, as server will change
+	$('#1_list').empty();
+	$('#2_list').empty();
+
+	$.ajax({
+		type: "POST",
+		url: "getPlayers.php",
+		data: {
+			game_id: game_id
+		},
+		dataType: 'json',
+		success: function(response){
+			var arr = response['results'];
+
+			for(var i=0; i < arr.length; i++){
+				var item = arr[i];
+				var team = item[0];
+				var name = item[1];
+
+				if(team == 1){
+					$('#1_list').append("<li>"+name+"</li>");
+				}else if(team == 2){
+					$('#2_list').append("<li>"+name+"</li>");
+				}
+			}
+
+		},
+		error: function(response){
+			alert("Something went wrong.  We REALLY suck. =(");
+		}
+	});
+}
+
+function bindEvents()
+{
+	$('select').on("change", function(){
+		game_id = $('select').find(":selected").attr("value");
+		populatePlayers();
 	});
 }
