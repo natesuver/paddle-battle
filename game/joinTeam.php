dc9d292f5e53f6e2881a1568ea1f75125a553504
Mike Peal <michaelcpeal@gmail.com>
2017-10-23 17:24:12 -0400

    joinTeam should work now
    Not tested with $_SESSION['username'], as code was not yet committed to project.  Tested and working with username = 'stif' (user_id = 62)

--- game/joinTeam.php
+++ game/joinTeam.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['username'])) {
 	header( 'Location: ../game/lobby.php' ); //user is already logged in, go right to lobby.
 }
 
-function returnFalse
+function returnFalse()
 {
 	$response = array(
 		"success" => false
@@ -17,35 +17,43 @@ function returnFalse
 /*
 first, get the user id for the user
 */
+$username = $_SESSION['username'];
+//$username = 'stif'; //test
+
 $conn = getConnection();
-$query = "SELECT id from users where username = '$_SESSION["username"]'";
+$query = "SELECT id from users where username = '$username'";
 
 $res = mysqli_query($conn, $query);
 
-if($!res){
+if(!$res){
 	returnFalse();
 	exit;
 }else{
-	print($res);
-	/*
-	now add user to a team using user id
-	*/
+	$user_id = '';
 
-	$user_id = 4; //testing
+	//assign $user_id
+	while($row = mysqli_fetch_row($res)){
+		$user_id = $row[0];
+	}
+
 	$team = $_POST['team'];
+	//$team = 1; //test
 	$game_id = $_POST['game_id'];
+	//$game_id = 1; //test
 
 	//look to see if the user has already joined a team
 	$query = "SELECT * from teams where game_id = '$game_id' and user_id = '$user_id'";
 	$exists = mysqli_query($conn, $query);
 
+	//if query doesn't go through, exit
 	if(!$exists){
 		returnFalse();
 		exit;
 	}
 
 	if(mysqli_num_rows($exists) == 1){
-		//if user has already joined team in this game, UPDATE team
+		//if user has already joined a team in this game, UPDATE team
+		echo "he's already on a team";
 		$query = "UPDATE teams set team = '$team' where game_id = '$game_id' and user_id = '$user_id'";
 		$exec = mysqli_query($conn, $query);
 
@@ -59,9 +67,10 @@ if($!res){
 			returnFalse();
 			exit;
 		}
-	}elseif (mysqli_num_rows($exists) == 0) {
+	}elseif(mysqli_num_rows($exists) == 0){
+		echo "new team member";
 		//if user has not joined a team in this game, INSERT team
-		$query = "INSERT INTO teams VALUES ('$game_id', '$team', '$user_id')";
+		$query = "INSERT INTO teams VALUES ('$user_id', '$team', '$game_id')";
 		$exec = mysqli_query($conn, $query);
 
 		if($exec){
@@ -75,6 +84,7 @@ if($!res){
 			exit;
 		}
 	}else{
+		//if num_rows is not 0 or 1, exit, something went wrong
 		returnFalse();
 		exit;
 	}
