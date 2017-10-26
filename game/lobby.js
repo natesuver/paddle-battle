d1515f3fc4529ad5129834c530c02688b9ab891b
Mike Peal <michaelcpeal@gmail.com>
2017-10-25 21:48:35 -0400

    adding very imperfect polling
    implemented polling that will update joined players on the fly.  it works, mostly, but there is some interference when you switch servers.

--- game/lobby.js
+++ game/lobby.js
@@ -1,6 +1,9 @@
 var game_id;
+var first_load;
+var pollingInterval;
 
 $(document).ready(function(){
+	first_load = true;
 	fillSelectBox();
 
 	bindEvents();
@@ -51,7 +54,11 @@ function fillSelectBox()
 
 function populatePlayers()
 {
-	showSpinner();
+	if(first_load){
+		showSpinner();
+		first_load = false;
+	}
+
 	//clear out ordered lists, as server will change
 	$('#1_list').empty();
 	$('#2_list').empty();
@@ -77,9 +84,11 @@ function populatePlayers()
 				}else if(team == 2){
 					$('#2_list').append("<li>"+name+"</li>");
 				}
+
+				showStartButton();
+				hideSpinner();
 			}
-			hideSpinner();
-			}else{
+		}else{
 				alert("Something went wrong.  We REALLY suck. =(");
 			}
 		},
@@ -89,8 +98,101 @@ function populatePlayers()
 	});
 }
 
+function pollForPlayers()
+{
+	$.ajax({
+		type: "POST",
+		url: "getPlayers.php",
+		data: {
+			game_id: game_id
+		},
+		dataType: 'json',
+		success: function(response){
+			if(response['success'] == true){
+			var arr = response['results'];
+			var polledNames1 = [];
+			var polledNames2 = [];
+			var team1Names = [];
+			var team2Names = [];
+
+			//here, we're getting the names of each player pulled in from php and adding them to respective arrays
+			for(var i=0; i < arr.length; i++){
+				var item = arr[i];
+				var team = item[0];
+				var name = item[1];
+
+				if(team == 1){
+					polledNames1.push(name);
+				}else if(team == 2){
+					polledNames2.push(name);
+				}
+			}
+
+			//now we grab the actual names that are currently in the <ol>s and turn them into arrays
+			$('#1_list li').each(function(i, elem) {
+    			team1Names.push($(elem).text());
+			});
+
+			$('#2_list li').each(function(i, elem) {
+    			team2Names.push($(elem).text());
+			});
+
+			//if there's any differences in the team1 array, re-populate team 1 list
+			var team1comp = compareNames(team1Names, polledNames1);
+			if(!team1comp){
+				$('#1_list').empty();
+				for(var i=0; i < polledNames1.length; i++){
+					var item = polledNames1[i];
+					$('#1_list').append("<li>"+item+"</li>");
+				}
+			}
+
+			var team2comp = compareNames(team2Names, polledNames2);
+			//if there's any differences in the team2 array, re-populate team 2 list
+			if(!team2comp){
+				$('#2_list').empty();
+				for(var i=0; i < polledNames2.length; i++){
+					var item = polledNames2[i];
+					$('#2_list').append("<li>"+item+"</li>");
+				}
+			}
+		}else{
+				alert("Something went wrong.  We REALLY suck. =(");
+			}
+		},
+		error: function(response){
+			alert("Something went wrong.  We REALLY suck. =(");
+		}
+	});
+}
+
+function compareNames(displayed_names, polled_names)
+{
+	var count = 0;
+	if(displayed_names.length != polled_names.length){
+		return false;
+	}
+
+	for(var i = 0; i < polled_names; i++){
+		var disp = displayed_names[i];
+		var poll = polled_names[i];
+
+		if(disp != poll){
+			count++;
+		}
+	}
+
+	if(count != 0){
+		return false;
+	}else{
+		return true;
+	}
+}
+
 function joinTeam1()
 {
+	stopPolling();
+	showSpinner();
 	$.ajax({
 		type: "POST",
 		url: "joinTeam.php",
@@ -102,7 +204,9 @@ function joinTeam1()
 			var response = JSON.parse(response);
 			if(response['success'] == true){
 				populatePlayers();
-				showLeaveButton();	
+				showLeaveButton();
+				hideSpinner();
+				startPolling();
 			}else{
 				alert("Something went wrong.  We REALLY suck. =(");
 			}
@@ -115,6 +219,8 @@ function joinTeam1()
 
 function joinTeam2()
 {
+	stopPolling();
+	showSpinner();
 	$.ajax({
 		type: "POST",
 		url: "joinTeam.php",
@@ -126,7 +232,9 @@ function joinTeam2()
 			var response = JSON.parse(response);
 			if(response['success'] == true){
 				populatePlayers();
-				showLeaveButton();	
+				showLeaveButton();
+				hideSpinner();
+				startPolling();
 			}else{
 				alert("Something went wrong.  We REALLY suck. =(");
 			}
@@ -139,6 +247,7 @@ function joinTeam2()
 
 function leaveTeam()
 {
+	stopPolling();
 	showSpinner();
 	$.ajax({
 		type: "POST",
@@ -151,6 +260,7 @@ function leaveTeam()
 			if(response['success'] == true){
 				populatePlayers();
 				hideLeaveButton();
+				startPolling();
 			}else{
 				alert("Something went wrong.  We REALLY suck. =(");
 			}
@@ -171,12 +281,38 @@ function hideLeaveButton()
 	$('#leaveTeam').hide();
 }
 
+function showStartButton()
+{
+	var list1 = $('#1_list');
+	var list2 = $('#2_list');
+
+	if(list1.length > 0 && list2.length > 0){
+		$('#start_button').show();
+	}else{
+		$('#start_button').hide();
+	}
+}
+
+function startPolling()
+{
+	pollingInterval = setInterval(pollForPlayers, 3000);
+}
+
+function stopPolling()
+{
+	clearInterval(pollingInterval);
+	console.log(pollingInterval);
+}
+
 function bindEvents()
 {
 	//when server select box changes, populate it with players currently joined to game
 	$('select').on("change", function(){
+		stopPolling();
 		game_id = $('select').find(":selected").attr("value");
 		populatePlayers();
+
+		startPolling();
 		$('#Teams').show();
 	});
 
