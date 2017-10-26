c54e199f9d233e7ecbd156c84d9352af6919b44c
Mike Peal <michaelcpeal@gmail.com>
2017-10-25 20:36:14 -0400

    closing connection

--- game/getPlayers.php
+++ game/getPlayers.php
@@ -12,6 +12,7 @@ function returnFalse()
 		"success" => false
 	);
 	echo json_encode($response);
+	$conn->close();
 }
 
 $conn = getConnection();
@@ -33,6 +34,7 @@ if(!$res){
 		"results" => $arr
 		);
 	echo json_encode($response);
+	$conn->close();
 	exit;
 }
 ?>
