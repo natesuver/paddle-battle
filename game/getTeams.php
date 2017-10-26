27536d3c5c0f0d47edf94e12f84440464a8b6e77
Nate Suver <nate.suver@gmail.com>
2017-10-25 22:12:07 -0400

    fix merge issue

--- game/getTeams.php
+++ game/getTeams.php
@@ -1,7 +1,6 @@
 <?php
 require '../common/database.php';
 
-$query = "SELECT teams.team, users.username from teams join users where teams.user_id = users.id and game_id = '$game_id'";
 $query = "SELECT teams.team, users.username, users.id from teams join users where teams.user_id = users.id and game_id = '$game_id'";
 $result = execResults($query);
 echo json_encode($result);
