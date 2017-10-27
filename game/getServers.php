c31259bb1d98d8a044b457fd87a51fc548515561
Mike Peal <michaelcpeal@gmail.com>
2017-10-26 21:38:30 -0400

    cleanup

--- game/getServers.php
+++ game/getServers.php
@@ -2,9 +2,6 @@
 require '../common/database.php';
 
 session_start();
-if (isset($_SESSION['username'])) {
-	header( 'Location: ../game/lobby.php' ); //user is already logged in, go right to lobby.
-}
 
 function returnFalse()
 {
@@ -12,17 +9,18 @@ function returnFalse()
 		"success" => false
 	);
 	echo json_encode($response);
-	$conn->close();
 }
 
 $conn = getConnection();
 
-$query = "SELECT server_id, server_name, url, game_id FROM servers where game_started = false";
+$query = "SELECT id, server_name, url, game_id FROM servers where game_started = false";
 
 $res = mysqli_query($conn, $query);
 
 if(!$res){
 	returnFalse();
+	$res->close();
+	$conn->close();
 	exit;
 }else{
 	$arr = mysqli_fetch_all($res);
@@ -32,6 +30,7 @@ if(!$res){
 		"results" => $arr
 		);
 	echo json_encode($response);
+	$res->close();
 	$conn->close();
 	exit;
 }
